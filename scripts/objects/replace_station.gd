05a4cbeb34628078742566eaa5677412410c4c6e
AntDum <antoine.dumoulin@student.uclouvain.be>
2025-08-10 19:39:30 +0200

    fix replace

--- scripts/objects/replace_station.gd
+++ scripts/objects/replace_station.gd
@@ -13,20 +13,28 @@ func burn() -> void:
 	replace()
 
 func replace() -> void:
-	print("REPLACE", is_destroyed)
 	if is_destroyed: return
 	rails.set_rail_at(coord)
 	is_destroyed = true
+	_hide_child()
 
 func restore() -> void:
-	print("REPLACE", is_destroyed)
 	if not is_destroyed: return
 	rails.set_cell(coord, 0, rock_atlas_coord)
 	is_destroyed = false
+	_show_child()
 
 func _restart() -> void:
 	if is_destroyed:
 		restore()
+
+func _hide_child() -> void:
+	for child in get_children():
+		child.visible = false
+
+func _show_child() -> void:
+	for child in get_children():
+		child.visible = true
 
 func _enter_tree() -> void:
 	EventBus.restart.connect(_restart)
